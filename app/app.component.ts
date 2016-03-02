1508b42d384f4692e1a17e2756c8f6983c41a90b
Kalpana Madhalam <mkalpanais@gmail.com>
2016-03-02 22:09:53 +1100

    Added a hero interface and used it in component for hero.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,29 @@
 import { Component } from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Heroes</h1>'
+    template:`
+            <h1>{{title}}</h1>
+            <h2>{{hero.name}} details!</h2>
+            <div>
+                <label>id: </label>{{hero.id}}
+            </div>
+            <div>
+                <label>name: </label>{{hero.name}}
+                <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+            </div>
+            `
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
