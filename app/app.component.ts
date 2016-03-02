eee5cea480b341056551c6235a1551cc2713d917
Kalpana Madhalam <mkalpanais@gmail.com>
2016-03-02 21:39:48 +1100

    Fixed the title of the app.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Quickstart</h1>'
+    template: '<h1>Angular 2 Heroes</h1>'
 })
 
 export class AppComponent { }
