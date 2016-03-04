0ae923f8cf9606085ba556252a496b4b6d6888e1
Kalpana Madhalam <mkalpanais@gmail.com>
2016-03-04 22:01:55 +1100

    Added a hero detail component to hold the selected hero details.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from 'angular2/core';
-
-interface Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
   selector: 'my-app',
+  directives: [HeroDetailComponent],
   styles: [`
     .selected {
         background-color: #CFD8DC !important;
@@ -66,16 +64,7 @@ interface Hero {
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div>
-          <label>id: </label>{{selectedHero.id}}
-      </div>
-      <div>
-          <label>name: </label>{{selectedHero.name}}
-          <div><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `
 })
 
